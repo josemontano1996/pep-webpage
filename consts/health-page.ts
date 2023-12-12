f4934fca07d37345e4e9462d098e078b7cc81b95
Jose Manuel Montano Mengual <josemanuel1996enologia@gmail.com>
2023-12-12 13:59:12 +0100

    fixing images path bug

--- consts/health-page.ts
+++ consts/health-page.ts
@@ -1,18 +1,18 @@
-import Image1 from '@/public/imgs/health/health-image-1.jpg';
-import Image2 from '@/public/imgs/health/health-image-2.jpg';
-import Image3 from '@/public/imgs/health/health-image-3.jpg';
-import Image4 from '@/public/imgs/health/health-image-4.jpg';
-import Image5 from '@/public/imgs/health/health-image-5.jpg';
-import Image6 from '@/public/imgs/health/health-image-6.jpg';
-import Image7 from '@/public/imgs/health/health-image-7.jpg';
-import Image8 from '@/public/imgs/health/health-image-8.jpg';
-import Image9 from '@/public/imgs/health/health-image-9.jpg';
-import Image10 from '@/public/imgs/health/health-image-10.jpg';
-import Image11 from '@/public/imgs/health/health-image-11.jpg';
-import Image12 from '@/public/imgs/health/groupImage.jpg';
+import Image1 from '../public/imgs/health/health-image-1.jpg';
+import Image2 from '../public/imgs/health/health-image-2.jpg';
+import Image3 from '../public/imgs/health/health-image-3.jpg';
+import Image4 from '../public/imgs/health/health-image-4.jpg';
+import Image5 from '../public/imgs/health/health-image-5.jpg';
+import Image6 from '../public/imgs/health/health-image-6.jpg';
+import Image7 from '../public/imgs/health/health-image-7.jpg';
+import Image8 from '../public/imgs/health/health-image-8.jpg';
+import Image9 from '../public/imgs/health/health-image-9.jpg';
+import Image10 from '../public/imgs/health/health-image-10.jpg';
+import Image11 from '../public/imgs/health/health-image-11.jpg';
+import Image12 from '../public/imgs/health/groupImage.jpg';
 
-import Oxigen1 from '@/public/imgs/health/oxigen-plant/oxigen-plant-1.png';
-import Oxigen3 from '@/public/imgs/health/oxigen-plant/oxigen-plant-3.png';
+import Oxigen1 from '../public/imgs/health/oxigen-plant/oxigen-plant-1.png';
+import Oxigen3 from '../public/imgs/health/oxigen-plant/oxigen-plant-3.png';
 
 export const chirurgicImages = [
   {
